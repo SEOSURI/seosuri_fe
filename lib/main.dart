0e61ac14e28cc562f50767936a7330f523d32278
noeyah2 <hy0708@cau.ac.kr>
2023-03-18 16:57:37 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class Seosuri extends StatelessWidget {
+  const Seosuri({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+
+    );
+  }
+}
